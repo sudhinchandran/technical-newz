8f267f1e3c0132040228781da5d8512c1252d82e
sudhinchandran <sudhinpathiyil@gmail.com>
2021-07-25 14:44:38 +0530

    Initial commit

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div>
       <h1 className={headerStyles.title}>
-        <span>WebDev</span> News
+        <span>Technical</span> News
       </h1>
       <p className={headerStyles.description}>
-        Keep up to date with the latest web dev news
+        Keep up to date with the latest Technical news
       </p>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -20,13 +20,4 @@ export const getStaticProps = async () => {
   }
 }
 
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-//   const articles = await res.json()
 
-//   return {
-//     props: {
-//       articles,
-//     },
-//   }
-// }
